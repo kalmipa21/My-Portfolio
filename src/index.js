be66290e279ec94580b1297af82618f5a0f83684
kalmipa21 <aripsapu@gmail.com>
2023-08-30 21:18:08 +0700

    import library bootstrap

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./routers/index.js";
 import reportWebVitals from "./reportWebVitals";
+
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+import "./assets/css/typography.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
