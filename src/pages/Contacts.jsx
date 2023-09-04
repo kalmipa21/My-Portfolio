68c384e9fbed03f36815e6910018a5861c1552d7
kalmipa21 <aripsapu@gmail.com>
2023-09-04 14:33:46 +0700

    Features: add contact page and connect to emailJS

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,9 +1,69 @@
-import { Container } from "react-bootstrap";
+import { Button, Container, Form } from "react-bootstrap";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 export default function Contacts() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_245bejf",
+        "template_otlujib",
+        form.current,
+        "uvkQVrJHrLTaCLsc4"
+      )
+      .then(
+        (result) => {
+          e.target.reset();
+          alert("Email Sent!");
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
-    <Container className="paddingpages">
-      <div></div>
+    <Container className="d-block paddingpages justify-content-center">
+      <h1 className="text-center">Contact Me</h1>
+      <h4 className="text-center text__1">
+        Please fill out the form below to discuss any work opportunity
+      </h4>
+      <Form ref={form} onSubmit={sendEmail} className="form_style mx-auto">
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label>Name :</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Your Name"
+            name="user_name"
+            className="areatype"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label>Email address :</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="name@example.com"
+            name="user_email"
+            className="areatype"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+          <Form.Label>Messages :</Form.Label>
+          <Form.Control
+            as="textarea"
+            rows={3}
+            name="message"
+            className="areatype"
+          />
+        </Form.Group>
+        <Button type="submit" value="Send">
+          Send
+        </Button>
+      </Form>
     </Container>
   );
 }
